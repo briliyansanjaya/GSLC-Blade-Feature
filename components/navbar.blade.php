6311a60496280836be3c55a6d634347a7d69b359
Briliyan Sanjaya Hartono <104523406+briliyansanjaya@users.noreply.github.com>
2022-10-31 19:47:57 +0700

    Delete components directory

--- components/navbar.blade.php
+++
@@ -1,7 +0,0 @@
-<nav class="navbar" style="height: 60px; background-color: 1B2733">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">
-            <img src="https://earlygame-prod.cdn.earlygame.com/images/Valorant-Logo_2021-09-17-094641_jsqj.png" alt="Valorant" width="135" height="40">
-        </a>
-    </div>
-</nav>
